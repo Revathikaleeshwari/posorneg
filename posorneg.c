2f3f0c5535fca958dfc2094978601ed71d81972c
Revathikaleeshwari <nrevathibe38@gmail.com>
2016-07-01 14:55:53 +0530

    Create posorneg.c

---
+++ posorneg.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("Enter a number\n");
+scanf("%d",&a);
+if(a>0)
+printf("%d is positive number\n",a);
+else
+printf("%d is negative number\n",a);
+return 0;
+}
